68cc6246d83ff3ae67d9f85ddf73f459fa2fc11e
32981gustavo <guga3.gustavo@gmail.com>
2024-12-05 16:43:13 +0000

    Add files via upload

---
+++ Eletronic Mall/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Eletronic_Mall
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Seguinte = new System.Windows.Forms.Button();
+            this.Welcome = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(271, 255);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(196, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(271, 281);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(196, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // Seguinte
+            // 
+            this.Seguinte.Location = new System.Drawing.Point(294, 334);
+            this.Seguinte.Name = "Seguinte";
+            this.Seguinte.Size = new System.Drawing.Size(153, 38);
+            this.Seguinte.TabIndex = 2;
+            this.Seguinte.Text = "Seguinte";
+            this.Seguinte.UseVisualStyleBackColor = true;
+            this.Seguinte.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Welcome
+            // 
+            this.Welcome.AutoSize = true;
+            this.Welcome.Location = new System.Drawing.Point(348, 105);
+            this.Welcome.Name = "Welcome";
+            this.Welcome.Size = new System.Drawing.Size(52, 13);
+            this.Welcome.TabIndex = 3;
+            this.Welcome.Text = "Welcome";
+            this.Welcome.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Welcome);
+            this.Controls.Add(this.Seguinte);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button Seguinte;
+        private System.Windows.Forms.Label Welcome;
+    }
+}
+
